48e3cdaf109b8cfcbaac3ed0250ea5571a617dfb
Albert Martinez <albert.martinez@csuc.cat>
2017-10-19 18:03:50 +0200

    Added folder and charset options in runnable command line for Validation-Core

--- Validation-Core/src/main/java/org/Validation/Core/App.java
+++ Validation-Core/src/main/java/org/Validation/Core/App.java
@@ -1,9 +1,12 @@
 package org.Validation.Core;
 
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -23,6 +26,7 @@ public class App {
 	private static Logger logger = LogManager.getLogger("Validation-Core");
 	
 	private static String file;
+	private static String charset = StandardCharsets.UTF_8.toString();
 	
 	private static AtomicInteger providedCHOSize = new AtomicInteger(0);
 	private static AtomicInteger aggregationSize = new AtomicInteger(0);
@@ -38,41 +42,53 @@ public class App {
 	 * @throws Exception
 	 */
     public static void main( String[] args ) throws Exception {    	
+    	if(Objects.nonNull(args) && args.length != 0)        		
+    		for(int i = 0; i < args.length; i++) {
+    			if(args[i].equals("--file"))	file = args[i+1];
+    			if(args[i].equals("--charset"))	charset = args[i+1];
+    		}
+    	if(Objects.isNull(file)) throw new Exception("file must not be null");
+		
     	try {
-        	if(Objects.nonNull(args) && args.length != 0)        		
-        		for(int i = 0; i < args.length; i++)	if(args[i].equals("--file"))	file = args[i+1];        		
-        	if(Objects.isNull(file)) throw new Exception("file must not be null");
-    		
-    		File f = new File(file);
-    		
-			Validate validate = 
-					new Validate(new FileInputStream(f), "UTF-8", RDF.class);
-			
-			logger.info(String.format("%s isValid %s", f.getName(), validate.isValid()));
-			
-			if(validate.isValid()) {			
-				RDF rdf = (RDF) validate.getElement();
-				rdf.getChoiceList().forEach(c->{
-					if(c.ifProvidedCHO())	providedCHOSize.getAndIncrement();        		
-	        		if(c.ifAggregation())	aggregationSize.getAndIncrement();         			
-	        		if(c.ifWebResource())	webResourceSize.getAndIncrement();
-	        		if(c.ifAgent())	agentSize.getAndIncrement();
-	        		if(c.ifPlace()) placeSize.getAndIncrement();
-	        		if(c.ifTimeSpan()) timeSpanSize.getAndIncrement();
-	        		if(c.ifConcept()) ConceptSize.getAndIncrement();
-				});
+			Files.walk(Paths.get(file))				
+			.filter(Files::isRegularFile)
+			.filter(f-> f.toString().endsWith(".xml"))
+			.forEach(f->{
+				try {					
+					Validate validate = 
+							new Validate(new FileInputStream(f.toFile()), charset , RDF.class);
+					
+					logger.info(String.format("%s isValid %s", f.getFileName(), validate.isValid()));
+					
+					if(validate.isValid()) {			
+						RDF rdf = (RDF) validate.getElement();
+						rdf.getChoiceList().forEach(c->{
+							if(c.ifProvidedCHO())	providedCHOSize.getAndIncrement();        		
+			        		if(c.ifAggregation())	aggregationSize.getAndIncrement();         			
+			        		if(c.ifWebResource())	webResourceSize.getAndIncrement();
+			        		if(c.ifAgent())	agentSize.getAndIncrement();
+			        		if(c.ifPlace()) placeSize.getAndIncrement();
+			        		if(c.ifTimeSpan()) timeSpanSize.getAndIncrement();
+			        		if(c.ifConcept()) ConceptSize.getAndIncrement();
+						});
 
-				logger.info(
-					String.format("ProvidedCHO: %s WebResource: %s Agent: %s Place: %s TimeSpan: %s Concept: %s Aggregation: %s",
-						providedCHOSize.get(), webResourceSize.get(), agentSize.get(), placeSize.get(), timeSpanSize.get(), ConceptSize.get(),
-						aggregationSize.get()));
-				
-			}else {
-				logger.info(validate.getError());				
-			}
-		} catch (FileNotFoundException e) {
+						logger.info(
+							String.format("ProvidedCHO: %s WebResource: %s Agent: %s Place: %s TimeSpan: %s Concept: %s Aggregation: %s",
+								providedCHOSize.get(), webResourceSize.get(), agentSize.get(), placeSize.get(), timeSpanSize.get(), ConceptSize.get(),
+								aggregationSize.get()));
+						
+					}else {
+						logger.info(validate.getError());				
+					}	
+				} catch (FileNotFoundException e) {
+					logger.error(e);
+					
+				} 
+						
+			});
+		} catch (IOException e) {
 			logger.error(e);
-			
-		}    
+		}        
+		   
     }
 }
