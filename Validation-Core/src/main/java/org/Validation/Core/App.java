af6eda278424beacc81d224af16c60da6c0b39a9
Albert Martinez <albert.martinez@csuc.cat>
2017-11-15 13:16:25 +0100

    Added multithreading by files walk on Validation-Core

--- Validation-Core/src/main/java/org/Validation/Core/App.java
+++ Validation-Core/src/main/java/org/Validation/Core/App.java
@@ -8,6 +8,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Objects;
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.Validation.Core.deserialize.Validate;
@@ -28,14 +29,15 @@ public class App {
 	private static String file;
 	private static String charset = StandardCharsets.UTF_8.toString();
 	private static String sch;
+	private static int threads = 1;
 
-	private static AtomicInteger providedCHOSize = new AtomicInteger(0);
-	private static AtomicInteger aggregationSize = new AtomicInteger(0);
-	private static AtomicInteger webResourceSize = new AtomicInteger(0);
-	private static AtomicInteger agentSize = new AtomicInteger(0);
-	private static AtomicInteger placeSize = new AtomicInteger(0);
-	private static AtomicInteger timeSpanSize = new AtomicInteger(0);
-	private static AtomicInteger ConceptSize = new AtomicInteger(0);
+	private static AtomicInteger providedCHO = new AtomicInteger(0);
+	private static AtomicInteger aggregation = new AtomicInteger(0);
+	private static AtomicInteger webResource = new AtomicInteger(0);
+	private static AtomicInteger agent = new AtomicInteger(0);
+	private static AtomicInteger place = new AtomicInteger(0);
+	private static AtomicInteger timeSpan = new AtomicInteger(0);
+	private static AtomicInteger Concept = new AtomicInteger(0);
 
 	/**
 	 * 
@@ -51,62 +53,66 @@ public class App {
 					charset = args[i + 1];
 				if (args[i].equals("--sch"))
 					sch = args[i + 1];
+				if (args[i].equals("--threads"))
+					threads = Integer.parseInt(args[i + 1]);
 			}
 		if (Objects.isNull(file))
 			throw new Exception("file must not be null");
 
-		try {
-			Files.walk(Paths.get(file)).filter(Files::isRegularFile).filter(f -> f.toString().endsWith(".xml"))
-					.forEach(f -> {
-						try {
-							Validate validate = new Validate(new FileInputStream(f.toFile()), charset, RDF.class);
-
-							logger.info(String.format("%s isValid %s XML", f.getFileName(), validate.isValid()));
-
-							if (validate.isValid()) {
-								RDF rdf = (RDF) validate.getElement();
-								rdf.getChoiceList().forEach(c -> {
-									if (c.ifProvidedCHO())
-										providedCHOSize.getAndIncrement();
-									if (c.ifAggregation())
-										aggregationSize.getAndIncrement();
-									if (c.ifWebResource())
-										webResourceSize.getAndIncrement();
-									if (c.ifAgent())
-										agentSize.getAndIncrement();
-									if (c.ifPlace())
-										placeSize.getAndIncrement();
-									if (c.ifTimeSpan())
-										timeSpanSize.getAndIncrement();
-									if (c.ifConcept())
-										ConceptSize.getAndIncrement();
-								});
-
-								logger.info(String.format(
-										"ProvidedCHO: %s WebResource: %s Agent: %s Place: %s TimeSpan: %s Concept: %s Aggregation: %s",
-										providedCHOSize.get(), webResourceSize.get(), agentSize.get(), placeSize.get(),
-										timeSpanSize.get(), ConceptSize.get(), aggregationSize.get()));
-
-							} else {
-								logger.info(validate.getError());
-							}
-						} catch (FileNotFoundException e) {
-							logger.error(e);
-						}
-						if (Objects.nonNull(sch)) {
-							SchematronUtil schUtil = new SchematronUtil(new File(sch), f.toFile());
+		new ForkJoinPool(threads).submit(() -> {
+			try {
+				Files.walk(Paths.get(file)).filter(Files::isRegularFile).filter(f -> f.toString().endsWith(".xml"))
+						.forEach(f -> {
 							try {
-								if (!schUtil.isValid()) {
-									logger.info(String.format("%s isValid %s SCH", f.getFileName(), schUtil.isValid()));
-									logger.info(schUtil.getSVRLFailedAssert());
+								Validate validate = new Validate(new FileInputStream(f.toFile()), charset, RDF.class);
+
+								logger.info(String.format("%s isValid %s XML", f.getFileName(), validate.isValid()));
+
+								if (validate.isValid()) {
+									RDF rdf = (RDF) validate.getElement();
+									rdf.getChoiceList().forEach(c -> {
+										if (c.ifProvidedCHO())
+											providedCHO.getAndIncrement();
+										if (c.ifAggregation())
+											aggregation.getAndIncrement();
+										if (c.ifWebResource())
+											webResource.getAndIncrement();
+										if (c.ifAgent())
+											agent.getAndIncrement();
+										if (c.ifPlace())
+											place.getAndIncrement();
+										if (c.ifTimeSpan())
+											timeSpan.getAndIncrement();
+										if (c.ifConcept())
+											Concept.getAndIncrement();
+									});
+								} else {
+									logger.info(validate.getError());
 								}
-							} catch (Exception e) {
+							} catch (FileNotFoundException e) {
 								logger.error(e);
 							}
-						}
-					});
-		} catch (IOException e) {
-			logger.error(e);
-		}
+							if (Objects.nonNull(sch)) {
+								SchematronUtil schUtil = new SchematronUtil(new File(sch), f.toFile());
+								try {
+									if (!schUtil.isValid()) {
+										logger.info(
+												String.format("%s isValid %s SCH", f.getFileName(), schUtil.isValid()));
+										logger.info(schUtil.getSVRLFailedAssert());
+									}
+								} catch (Exception e) {
+									logger.error(e);
+								}
+							}
+						});
+			} catch (IOException e) {
+				logger.error(e);
+			}
+		}).join();
+
+		logger.info(String.format(
+				"ProvidedCHO: %s WebResource: %s Agent: %s Place: %s TimeSpan: %s Concept: %s Aggregation: %s",
+				providedCHO.get(), webResource.get(), agent.get(), place.get(), timeSpan.get(), Concept.get(),
+				aggregation.get()));
 	}
 }
