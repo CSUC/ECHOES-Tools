72e1420025a23301d34ea4c9fd9d34831821ce35
David Fernández Aldana <david.fernandez@csuc.cat>
2017-11-29 09:21:15 +0100

    Initial ApeEAD implementation

--- EDM-Transformations/src/main/java/org/EDM/Transformations/formats/ead/EAD2EDM.java
+++ EDM-Transformations/src/main/java/org/EDM/Transformations/formats/ead/EAD2EDM.java
@@ -1,11 +1,77 @@
 package org.EDM.Transformations.formats.ead;
 
+import eu.europeana.corelib.definitions.jibx.HasView;
+import eu.europeana.corelib.definitions.jibx.RDF;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import javax.xml.transform.stream.StreamSource;
+import org.EDM.Transformations.formats.xslt.XSLTTransformations;
+import org.EDM.Transformations.deserialize.JibxUnMarshall;
+import org.EDM.Transformations.serialize.JibxMarshall;
+
 /**
  * @author amartinez
  *
  */
 public class EAD2EDM {
 
-    public EAD2EDM(){}
+    private RDF rdf = null;
+    private final OutputStream out;
+
+    public EAD2EDM(String xslt, InputStream ins, Map<String, String> xsltProperties, OutputStream outs) throws IOException, Exception {
+        this.out = outs;
+        File tempFile = File.createTempFile("Echoes-Tools", ".edm");
+        tempFile.deleteOnExit();
+
+        XSLTTransformations xsltTrans = new XSLTTransformations(xslt, new FileOutputStream(tempFile), xsltProperties);
+        xsltTrans.transformationsFromSource(new StreamSource(ins));
+        
+        JibxUnMarshall unmarshall = new JibxUnMarshall(new FileInputStream(tempFile), "UTF-8", RDF.class);  
+        rdf = (RDF) unmarshall.getElement();
+    }
+
+    public RDF getRDF(){
+        return this.rdf;
+    }
+    
+    public void delAdditionalhasViewWebResources() {        
+        if (rdf != null) {
+            HashSet<String> urisToDelete = new HashSet<>();
+            List<RDF.Choice> elements = rdf.getChoiceList();
+            // We first collect the resources of edm:hasView to delete and delete them
+            elements.forEach(c -> {
+                if (c.ifAggregation()) {
+                    List<HasView> hasViewList = c.getAggregation().getHasViewList();
+                    if (hasViewList != null && hasViewList.size() > 1) {
+                        List<HasView> hasViewListToDel = hasViewList.subList(1, hasViewList.size());
+                        urisToDelete.addAll(hasViewListToDel.stream().map(
+                                h -> h.getResource())
+                                .collect(Collectors.toCollection(HashSet<String>::new)));
+                        hasViewList.removeAll(hasViewListToDel);
+                    }
+                }
+            });
+            // We remove the WebResource(s) which about are in deleted edm:hasView
+            rdf.setChoiceList(elements.parallelStream().filter(e -> !e.ifWebResource() || !urisToDelete.contains(e.getWebResource().getAbout()))
+                    .collect(Collectors.toCollection(ArrayList<RDF.Choice>::new)));
+        }
+    }
+
+    public void marshal(Charset encoding, boolean alone) {
+        if (Objects.nonNull(this) && !Objects.equals(this, new RDF())) {
+            JibxMarshall.marshall(rdf, encoding.toString(), alone, out, RDF.class);
+        }
+    }
 
 }
---
+++ EDM-Transformations/src/test/java/org/EDM/Transformations/formats/ead/EAD2EDMTest.java
@@ -0,0 +1,80 @@
+/**
+ *
+ */
+package org.EDM.Transformations.formats.ead;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.assertTrue;
+import isbn._1_931666_22_9.Ead;
+import org.EDM.Transformations.deserialize.JaxbUnmarshal;
+
+/**
+ * @author amartinez
+ *
+ */
+public class EAD2EDMTest extends TestCase {
+
+    /**
+     * Test method for
+     * {@link org.EDM.Transformations.formats.a2a.EAD2EDM#marshal(java.nio.charset.Charset, boolean)}.
+     *
+     * @throws java.io.FileNotFoundException
+     */
+    @Test
+    public void testMarshal() throws FileNotFoundException, Exception {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("ead/ead.xml").getFile());
+        assertTrue(file.exists());
+        JaxbUnmarshal jxb = new JaxbUnmarshal(file, new Class[]{Ead.class});
+        assertNotNull(jxb.getObject());
+        assertTrue(jxb.isValidating());
+        
+        EAD2EDM edm = new EAD2EDM(classLoader.getResource("ead/ead2edm.xsl").getPath(), new FileInputStream(file), properties(), System.out);
+
+        assertNotNull(edm);
+        assertNotNull(edm.getRDF());
+
+        edm.marshal(StandardCharsets.UTF_8, true);
+    }
+
+    // TODO: Test delAdditionalhasViewWebResources function with tweaked XSL
+    
+    
+    public Map<String, String> properties() {
+        Map<String, String> properties = new HashMap<>();
+        properties.put("europeana_provider", "europeana_provider");
+        properties.put("europeana_dataprovider", "europeana_dataprovider");
+        properties.put("europeana_rights", "europeana_rights");
+        properties.put("dc_rights", "dc_rights");
+        properties.put("europeana_type", "VIDEO");
+        properties.put("idSource", "unitid");
+        properties.put("useISODates", "false");
+        properties.put("language", "en");
+        properties.put("inheritElementsFromFileLevel", Boolean.TRUE.toString());
+        properties.put("inheritOrigination", Boolean.TRUE.toString());
+        properties.put("inheritUnittitle", Boolean.TRUE.toString());
+        properties.put("inheritLanguage", Boolean.TRUE.toString());
+        properties.put("inheritRightsInfo", Boolean.TRUE.toString());
+        properties.put("useExistingDaoRole", Boolean.TRUE.toString());
+        properties.put("useExistingLanguage", Boolean.TRUE.toString());
+        properties.put("useExistingRepository", Boolean.TRUE.toString());
+        properties.put("useExistingRightsInfo", Boolean.TRUE.toString());
+        properties.put("minimalConversion", Boolean.FALSE.toString());
+        properties.put("edm_identifier", "edm_identifier");
+        properties.put("host", "localhost");
+        properties.put("repository_code", "FR-SIAF");
+        properties.put("xml_type_name", "fa");
+        properties.put("landingPage", "ape");
+        properties.put("useArchUnittitle", Boolean.TRUE.toString());
+        return properties;
+    }
+}
