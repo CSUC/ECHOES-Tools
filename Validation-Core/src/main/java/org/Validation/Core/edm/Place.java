e12bb37b2fe6c0b0cbcccac687e2da07665fe0f4
Albert Martinez <albert.martinez@csuc.cat>
2018-01-18 12:54:25 +0100

    Fixed Place validation

--- Validation-Core/src/main/java/org/Validation/Core/edm/Place.java
+++ Validation-Core/src/main/java/org/Validation/Core/edm/Place.java
@@ -2,13 +2,13 @@ package org.Validation.Core.edm;
 
 import eu.europeana.corelib.definitions.jibx.PlaceType;
 import eu.europeana.corelib.definitions.jibx.RDF;
-import eu.europeana.corelib.definitions.jibx.WebResourceType;
 import org.Validation.Core.DataType;
 import org.Validation.Core.InterfaceCoreClasses;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 /**
@@ -26,8 +26,8 @@ public class Place extends DataType implements InterfaceCoreClasses<PlaceType> {
     @Override
     public PlaceType validate(PlaceType data) {
         aboutType(data.getAbout());        
-        floatType(data.getLat().toString());
-        floatType(data.getLong().toString());        
+        if(Objects.nonNull(data.getLat()))  floatType(data.getLat().toString());
+        if(Objects.nonNull(data.getLong()))  floatType(data.getLong().toString());
         Optional.ofNullable(data.getNoteList()).ifPresent(present-> present.forEach(note -> literalType(note)));
         Optional.ofNullable(data.getPrefLabelList()).ifPresent(present-> present.forEach(prefLabel -> literalType(prefLabel)));
         Optional.ofNullable(data.getAltLabelList()).ifPresent(present-> present.forEach(altLabel -> placeType(altLabel.getString())));
