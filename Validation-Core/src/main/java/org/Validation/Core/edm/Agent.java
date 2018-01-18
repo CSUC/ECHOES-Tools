3af56e54f4eb690672f4255bbeb5fa7fe3caf074
Albert Martinez <albert.martinez@csuc.cat>
2018-01-18 12:25:28 +0100

    Fixed Agent validation

--- Validation-Core/src/main/java/org/Validation/Core/edm/Agent.java
+++ Validation-Core/src/main/java/org/Validation/Core/edm/Agent.java
@@ -1,56 +1,45 @@
 package org.Validation.Core.edm;
 
 import eu.europeana.corelib.definitions.jibx.*;
-import org.Validation.Core.DataTypeAgent;
+import org.Validation.Core.DataType;
 import org.Validation.Core.InterfaceCoreClasses;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.time.Instant;
+import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 /**
  * @author dfernandez
  */
-public class Agent extends DataTypeAgent implements InterfaceCoreClasses<AgentType> {
+public class Agent extends DataType implements InterfaceCoreClasses<AgentType> {
 
     private static Logger logger = LogManager.getLogger(Agent.class);
     private Instant inici = Instant.now();
 
-    public Agent() {
-        super();
+    public Agent(List<RDF.Choice> listChoice) {
+        super(listChoice);
         logger.info("VALIDATION     {}", this.getClass().getSimpleName());
     }
 
     @Override
     public AgentType validate(AgentType data) {
-       
-        AboutType aboutType = new AboutType();
-        aboutType.setAbout(data.getAbout());
-        aboutType(aboutType);
-        
+        aboutType(data.getAbout());
+
         Optional.ofNullable(data.getPrefLabelList()).ifPresent(p -> p.forEach(prefLabelList -> literalType(prefLabelList)));
         Optional.ofNullable(data.getAltLabelList()).ifPresent(p -> p.forEach(altLabelList -> literalType(altLabelList)));
         Optional.ofNullable(data.getHasMetList()).ifPresent(p -> p.forEach(hasMetList -> resourceType(hasMetList)));
         Optional.ofNullable(data.getIsRelatedToList()).ifPresent(p -> p.forEach(isRelatedTo -> resourceOrLiteralType(isRelatedTo)));
-        // FIXME: Should dateOfBirth be a child class of DateType?
-        if (data.getDateOfBirth() != null){
-            if (dateOfBirthType(data.getDateOfBirth()) || literalType(data.getDateOfBirth())){
-                return null;
-            }
-        }
-        // FIXME: Should dateOfDate be a child class of DateType?
-        if (data.getDateOfDeath() != null){
-            if (dateOfDeathType(data.getDateOfDeath()) || literalType(data.getDateOfDeath())){
-                return null;
-            }
-        }
+
+        if(Objects.nonNull(data.getDateOfBirth())) dateType(data.getDateOfBirth());
+        if(Objects.nonNull(data.getDateOfDeath())) dateType(data.getDateOfDeath());
+
         Optional.ofNullable(data.getGender()).ifPresent(p -> literalType(p));
-        if (data.getPlaceOfBirth() != null){
-            if (placeOfBirthType(data.getPlaceOfBirth()) || resourceOrLiteralType(data.getPlaceOfBirth())){
-                return null;
-            }
-        }
+
+        if(Objects.nonNull(data.getPlaceOfBirth())) resourceOrLiteralType(data.getPlaceOfBirth());
+
         Optional.ofNullable(data.getProfessionOrOccupationList()).ifPresent(p -> p.forEach(professionOrOccupation -> resourceOrLiteralType(professionOrOccupation)));
         
         return null;
